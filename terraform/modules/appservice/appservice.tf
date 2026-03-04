60531aeb1e8c81ea06459f66e18e2fc896ee577c
sriveerapaneni <veerapanenikiranmaye@gmail.com>
2026-03-04 17:25:04 -0600

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,13 +1,8 @@
-resource "random_string" "app_suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
 resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
+  kind                = "Windows"
 
   sku {
     tier = "Basic"
@@ -16,12 +11,17 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "${var.application_type}-${var.resource_type}-${random_string.app_suffix.result}"
+  name                = "${var.application_type}-${var.resource_type}-${var.app_suffix}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   app_service_plan_id = azurerm_app_service_plan.test.id
 
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "None"
+  }
+
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 0
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 }
